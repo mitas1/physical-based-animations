f37df3133aeabe1b958385a7b36b45d8033b4cdb
Marian Skrip <marian.skrip@devinsolutions.com>
2017-10-07 18:29:11 +0200

    Implement basic slider for changing emiter's emission rate

---
+++ Assets/Scripts/ParticleSystemController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParticleSystemController : MonoBehaviour {
+
+    ParticleSystem.EmissionModule emissionModule;
+
+    void Start () {
+        emissionModule = gameObject.GetComponent<ParticleSystem> ().emission;
+    }
+
+    public void ChangeParticlesPerSecond(Slider slider) {
+        Debug.Log (slider.value);
+        emissionModule.enabled = true;
+        emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(slider.value);
+    }
+}
---
+++ Assets/Scripts/SliderLabels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderLabels : MonoBehaviour {
+
+    public Text minLabel;
+    public Text maxLabel;
+
+    void Start () {
+        Slider slider = gameObject.GetComponent<Slider> ();
+        minLabel.text = ((int) slider.minValue).ToString();
+        maxLabel.text = ((int) slider.maxValue).ToString();
+    }
+}
