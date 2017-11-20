1895051e49ea14ddf00f471d2b8bd4010f8b9b49
Samuel Mitas <s.mitas@devinprint.sk>
2017-11-20 17:08:27 +0100

    Initialize GUI handlers

---
+++ handlers.go
@@ -0,0 +1,13 @@
+package main
+
+var state = HandledOptions{
+	running: true,
+}
+
+func HandlePlayClick (state *HandledOptions) {
+	state.running = true
+}
+
+func HandlePauseClick (state *HandledOptions) {
+	state.running = false
+}
