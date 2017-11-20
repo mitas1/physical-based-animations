b4e81fea7d3030ec04b3e94597c046117439e624
Samuel Mitas <s.mitas@devinprint.sk>
2017-11-20 17:09:38 +0100

    Add Play and Pause Button

--- physical-based-animations.go
+++ physical-based-animations.go
@@ -13,6 +13,11 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 )
 
+const (
+	winWidth  = 1024
+	winHeight = 768
+)
+
 func updateParticles(particles []Particle, batch *pixel.Batch, dt float64, cam pixel.Matrix) {
 	for i := 0; i < len(particles); i++ {
 		newPos, err := newPosition(&particles[i], dt, Verlet)
@@ -52,17 +57,15 @@ func run() {
 
 	win.SetSmooth(true)
 
-	data, err := Asset("assets/sprites/particle.png")
-	if err != nil {
-		panic(err)
-	}
-
-	particleImage, err := loadPicture(data)
+	particleImage, err := loadPicture("assets/sprites/particle.png")
 	if err != nil {
 		panic(err)
 	}
 
 	batch := pixel.NewBatch(&pixel.TrianglesData{}, particleImage)
+
+
+
 	particleSprite := pixel.NewSprite(
 		particleImage,
 		pixel.R(
@@ -90,53 +93,89 @@ func run() {
 	prevDt := 0.002
 
 	last := time.Now()
+
+	gui := GUI{
+		win: win,
+	}
+
+	playButton := Button{
+		position: pixel.V(10, 10),
+		bounds: pixel.R(0, 0, 80, 80),
+		image: "assets/sprites/play.png",
+		onClick: HandlePlayClick,
+	}
+
+	gui.NewButton(playButton)
+
+	pauseButton := Button{
+		position:  pixel.V(10, 100),
+		bounds: pixel.R(0, 0, 80, 80),
+		image: "assets/sprites/pause.png",
+		onClick: HandlePauseClick,
+	}
+
+	gui.NewButton(pauseButton)
+
+	cam := pixel.IM.Scaled(camPos, 1.0).Moved(win.Bounds().Center().Sub(camPos))
+	
+	win.SetMatrix(cam)
+
+	gui.SetMatrix(cam)
+
+	gui.BindState(&state)
+	gui.MainLoop()
+
 	for !win.Closed() {
-		dt := time.Since(last).Seconds()
-		last = time.Now()
-		timeElapsed += dt
-
-		cam := pixel.IM.Scaled(camPos, 1.0).Moved(win.Bounds().Center().Sub(camPos))
-		win.SetMatrix(cam)
-		win.Clear(colornames.Whitesmoke)
-
-		batch.Clear()
-		updateParticles(particleSystem.particles, batch, dt, cam)
-
-		batch.Draw(win)
-
-		for timeElapsed > timeForOneParticle {
-			pos := particleSystem.position
-			angle := (rand.Float64() - 0.5) * (particleSystem.angle * (math.Pi / 180))
-			speed := pixel.V(0, startSpeed).Rotated(angle)
-			nextPost := pos.Add(speed.Scaled(PixelsPerMeter).Scaled(prevDt)).Add(Gravity.Scaled(PixelsPerMeter).Scaled(prevDt * prevDt * 0.5))
-			// fmt.Printf("pos: %f %f	prev: %f %f\n", pos.X, pos.Y, prevPos.X, prevPos.Y)
-			particle := Particle{
-				position:     pos,
-				nextPosition: nextPost,
-				speed:        speed,
-				prevDt:       prevDt,
-				sprite:       *particleSprite,
-				lifespan:     100.0,
-				alive:        0.0,
-			}
-			particleSystem.particles = append(particleSystem.particles, particle)
-			timeElapsed = timeElapsed - timeForOneParticle
-		}
 
 		win.Update()
 
-		frames++
-		select {
-		case <-second:
-			particleSystem.KillOldParticles(
-				win.Bounds().Min.X,
-				win.Bounds().Max.X,
-				win.Bounds().Min.Y,
-			)
-			win.SetTitle(fmt.Sprintf("%s | FPS: %d | particles %d", cfg.Title, frames,
-				len(particleSystem.particles)))
-			frames = 0
-		default:
+		if (gui.GetState().running) {
+			win.Clear(colornames.Whitesmoke)
+			batch.Clear()
+
+			dt := time.Since(last).Seconds()
+			last = time.Now()
+			timeElapsed += dt
+
+			updateParticles(particleSystem.particles, batch, dt, cam)
+
+			batch.Draw(win)
+			gui.Draw()
+
+
+			for timeElapsed > timeForOneParticle {
+				pos := particleSystem.position
+				angle := (rand.Float64() - 0.5) * (particleSystem.angle * (math.Pi / 180))
+				speed := pixel.V(0, startSpeed).Rotated(angle)
+				nextPost := pos.Add(speed.Scaled(PixelsPerMeter).Scaled(prevDt)).Add(
+					Gravity.Scaled(PixelsPerMeter).Scaled(prevDt * prevDt * 0.5))
+				// fmt.Printf("pos: %f %f	prev: %f %f\n", pos.X, pos.Y, prevPos.X, prevPos.Y)
+				particle := Particle{
+					position:     pos,
+					nextPosition: nextPost,
+					speed:        speed,
+					prevDt:       prevDt,
+					sprite:       *particleSprite,
+					lifespan:     100.0,
+					alive:        0.0,
+				}
+				particleSystem.particles = append(particleSystem.particles, particle)
+				timeElapsed = timeElapsed - timeForOneParticle
+			}
+	
+			frames++
+			select {
+			case <-second:
+				particleSystem.KillOldParticles(
+					win.Bounds().Min.X,
+					win.Bounds().Max.X,
+					win.Bounds().Min.Y,
+				)
+				win.SetTitle(fmt.Sprintf("%s | FPS: %d | particles %d", cfg.Title, frames,
+					len(particleSystem.particles)))
+				frames = 0
+			default:
+			}
 		}
 	}
 }
